449669bb3957b6d7cdd86ec80b1c07dd4eaa1cc2
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2012-12-17 19:12:03 +0100

    style

---
+++ web.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+app.configure(function () {  
+  // Show Errors Oppan Java Style
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  
+  // Static Files Folder
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(request, response) {
+	response.setHeader("Content-Type", "text/html");
+	response.sendfile('index.html');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
