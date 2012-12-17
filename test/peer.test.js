19f66e00321e3be1a518b2c337af00ca52472637
Pasquale Boemio <boemianrapsodi@gmail.com>
2012-12-17 22:52:25 +0100

    tests mock

---
+++ test/peer.test.js
@@ -0,0 +1,24 @@
+require('should');
+var assert = require('assert');
+
+describe('Peer:\n', function(){
+	beforeEach(function(done){
+		done();
+	});
+
+	afterEach(function(done){
+		done();
+	});
+
+	before(function(done){
+		done();
+	});
+
+	after(function(done){
+		done();
+	});
+
+	it('Dummy Test', function(done){
+		done();
+	});
+});
