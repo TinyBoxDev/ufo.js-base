123e7a5202ad949480dc7b2d3c1d1793a51d2edc
Pasquale Boemio <boemianrapsodi@gmail.com>
2012-12-19 14:44:41 +0100

    bdd started

--- public/js/peer.js
+++ public/js/peer.js
@@ -1,6 +1,9 @@
-function Peer() {
+var Peer = function() {
+	this.channel = null;
 }
 
 //Apple.prototype.getInfo = function() {
 //    return this.color + ' ' + this.type + ' apple';
 //};
+
+module.exports.Peer = Peer;
