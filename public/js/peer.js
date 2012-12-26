c754cdf002f574ac3140b7584078a58fc95da236
Pasquale Boemio <boemianrapsodi@gmail.com>
2012-12-26 10:21:07 +0100

    self into this :)

--- public/js/peer.js
+++ public/js/peer.js
@@ -1,36 +1,37 @@
-var self = null;
-
 var Peer = function(bootstrapServerAddress) {
+	var self = this;
+
+	/* Step 2I of the peering process: take the answer received and instantiate a datachannel */
+	var onPeeringReply = function(reply) {
+		self.peerConnection.setRemoteDescription(reply.answer);
+		this.connectToPeer(self.peerConnection);
+	}
+	
 	this.peerConnection = null;
 
 	this.channel = new Channel();
 	this.channel.on('peeringReply', onPeeringReply);
 	this.channel.connectByName(bootstrapServerAddress);
-	self = this;
 }
 
 Peer.prototype.lookForAPeer = function() {
+	var self = this;
+
+	/* Step 0I of the peering process: set a fake audio stream */
+	var onFakeStreamDone = function(stream) {
+		self.peerConnection.addStream(stream);
+		self.peerConnection.createOffer(onOfferCreated, generalFailureCallback);
+	}
+
+	/* Step 1I of the peering process: prepare an offer and send it to the bootstrap server */
+	var onOfferCreated = function(offer) {
+		var packet = new p2pPacket('peering', new peeringPacket(offer));
+		self.peerConnection.setLocalDescription(offer);
+		self.channel.send(packet);
+	}
+
 	this.peerConnection = new mozRTCPeerConnection();
 	navigator.mozGetUserMedia({audio:true, fake:true}, onFakeStreamDone, generalFailureCallback);	
-}
-
-/* Step 0I of the peering process: set a fake audio stream */
-var onFakeStreamDone = function(stream) {
-	self.peerConnection.addStream(stream);
-	self.peerConnection.createOffer(onOfferCreated, generalFailureCallback);
-}
-
-/* Step 1I of the peering process: prepare an offer and send it to the bootstrap server */
-var onOfferCreated = function(offer) {
-	var packet = new p2pPacket('peering', new peeringPacket(offer));
-	self.peerConnection.setLocalDescription(offer);
-	self.channel.send(packet);
-}
-
-/* Step 2I of the peering process: take the answer received and instantiate a datachannel */
-var onPeeringReply = function(reply) {
-	self.peerConnection.setRemoteDescription(reply.answer);
-	this.connectToPeer(self.peerConnection);
 }
 
 var generalFailureCallback = function(errorMessage) {
