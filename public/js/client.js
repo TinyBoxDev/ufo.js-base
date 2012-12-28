a54c112591a56340a3adc5cf1c915194ef128c4d
Pasquale Boemio <boemianrapsodi@gmail.com>
2012-12-28 23:20:42 +0100

    client minor changes

--- public/js/client.js
+++ public/js/client.js
@@ -4,6 +4,16 @@ var Client = function() {
 	this.connectionPool = [];
 }
 
+Client.prototype.bootstrap = function() {
+	this.setViewDuringBootstrap();
+	var currentPeer = new Peer.Peer('http://p2pwebsharing.herokuapp.com');
+	currentPeer.lookForAPeer(this.onBootstrapPerformed);
+}
+
+Client.prototype.setViewDuringBootstrap = null;
+
+Client.prototype.onBootstrapPerformed = null;
+
 	exports.Client = Client;
 
 })(typeof exports === 'undefined'? this['Client']={}: exports);
--- public/js/peer.js
+++ public/js/peer.js
@@ -1,11 +1,11 @@
-if(!Channel)
-	var Channel = require('channel');
-if(!p2pPacket)
-	var p2pPacket = require('p2pPacket');
-if(!peeringPacket)
-	var peeringPacket = require('p2pPacket');
-if(!peeringReplyPacket) 
-	var peeringReplyPacket = require('peeringReplyPacket');
+//if(!Channel)
+//	var Channel = require('channel');
+//if(!p2pPacket)
+//	var p2pPacket = require('p2pPacket');
+//if(!peeringPacket)
+//	var peeringPacket = require('peeringPacket');
+//if(!peeringReplyPacket) 
+//	var peeringReplyPacket = require('peeringReplyPacket');
 
 (function(exports){
 var Peer = function(bootstrapServerAddress) {
@@ -13,18 +13,7 @@ var Peer = function(bootstrapServerAddress) {
 	this.channel = new Channel.Channel();
 	
 	if(bootstrapServerAddress) {
-		/* Step 2I of the peering process: take the answer received and instantiate a datachannel */
-		var onPeeringReply = function(reply) {
-			self.peerConnection.setRemoteDescription(reply.answer, connectToPeer);
-		}
-
-		/* Step 3I of the peering process: connect to peer */
-		var connectToPeer = function() {
-			self.peerConnection.connectDataConnection(5000,5001);
-		}		
-		
 		this.peerConnection = null;
-		this.channel.on('peeringReply', onPeeringReply);
 		this.channel.connectByName(bootstrapServerAddress);
 	}
 }
@@ -37,7 +26,7 @@ Peer.prototype.sendPeeringReply = function(answer) {
 	this.channel.send(new p2pPacket.p2pPacket('peeringReply', new peeringReplyPacket.peeringReplyPacket(answer)));
 }
 
-Peer.prototype.lookForAPeer = function() {
+Peer.prototype.lookForAPeer = function(onPeerFound) {
 	var self = this;
 
 	/* Step 0I of the peering process: set a fake audio stream */
@@ -53,20 +42,29 @@ Peer.prototype.lookForAPeer = function() {
 		self.channel.send(packet);
 	}
 	
+	/* Step 2I of the peering process: take the answer received and instantiate a datachannel */
+	var onPeeringReply = function(reply) {
+		if(reply.answer)
+			self.peerConnection.setRemoteDescription(reply.answer, connectToPeer);
+		else
+			onPeerFound();
+	}
 
+	/* Step 3I of the peering process: connect to peer */
+	var connectToPeer = function() {
+		self.peerConnection.connectDataConnection(5000,5001);
+	}	
+	
+	this.channel.on('peeringReply', onPeeringReply);	
 	this.peerConnection = new mozRTCPeerConnection();
 
 	/* Step 4I of the peering process: save data channel as new wrapped channel */
 	this.peerConnection.onconnection = function() {
-		console.log('onconnection');
 		self.channel.connectViaSocket(self.peerConnection.createDataChannel("Reliable data channel :)",{}));
+		onPeerFound();
 	}
 
 	navigator.mozGetUserMedia({audio:true, fake:true}, onFakeStreamDone, generalFailureCallback);	
-}
-
-Peer.prototype.setPeeringCallback = function(peeringCallback) {
-	this.channel.on('peering', peeringCallback);
 }
 
 var generalFailureCallback = function(errorMessage) {
