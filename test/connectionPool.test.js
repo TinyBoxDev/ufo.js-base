22b9c3bbf06d8a0684fee160c06d954db2e96605
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2013-01-03 13:49:28 +0100

    test bss

--- test/connectionPool.test.js
+++ test/connectionPool.test.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+
 describe('Connection Pool:\n', function() {
     var thisConnectionPool = null;
 
@@ -31,8 +33,57 @@ describe('Connection Pool:\n', function() {
     it('Should create pool with init values', function(done) {
 	var secondPool = new connectionPool(10);
 	thisConnectionPool.size.should.eql(4);
+	thisConnectionPool.usedConnections.should.eql(0);
 	secondPool.size.should.eql(10);
 	done();
     });
+
+    it('Should push new connection inside the pool', function(done) {
+	thisConnectionPool.pushConnection('myTestConnection', 'connectionBody');
+	thisConnectionPool.should.have.property('myTestConnection');
+	thisConnectionPool.myTestConnection.should.eql('connectionBody');
+	done();
+    });
+
+    it('Should not push new connection if pool is full', function(done) {
+	thisConnectionPool.pushConnection('myTestConnection1', 'connectionBody');
+	thisConnectionPool.pushConnection('myTestConnection2', 'connectionBody');
+	thisConnectionPool.pushConnection('myTestConnection3', 'connectionBody');
+	thisConnectionPool.pushConnection('myTestConnection4', 'connectionBody');
+	try {
+	    thisConnectionPool.pushConnection('myTestConnection5', 'connectionBody');
+	} catch(error) {
+	    done();
+	}
+    });
+
+    it('Should get existing connection from pool', function(done) {
+	thisConnectionPool.pushConnection('myTestConnection1', 'connectionBody');
+	assert('connectionBody' === thisConnectionPool.getConnectionByName('myTestConnection1'));
+	done();
+    });
+
+    it('Should not get not existing connection from pool', function(done) {
+	assert(!thisConnectionPool.getConnectionByName('myTestConnection1'));
+	done();
+    });
+
+    it('Should delete existing connection from pool', function(done) {
+	thisConnectionPool.pushConnection('myTestConnection1', 'connectionBody');
+	thisConnectionPool.deleteConnectionByName('myTestConnection1');
+	assert(!thisConnectionPool.getConnectionByName('myTestConnection1'));
+	thisConnectionPool.usedConnections.should.eql(0);
+	done();
+    });
+
+    it('Should manage deleting of unexistign connection from pool', function(done) {
+	thisConnectionPool.pushConnection('myTestConnection1', 'connectionBody');
+	thisConnectionPool.deleteConnectionByName('myTestConnection2');
+	assert(!thisConnectionPool.getConnectionByName('myTestConnection2'));
+	thisConnectionPool.usedConnections.should.eql(1);
+	done();
+    });
+
+    
 
 });
