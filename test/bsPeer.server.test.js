a5635ebda5f3ccdd959a0cf3b228e91e1cdd04c0
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2013-01-08 18:44:49 +0100

    routing

--- test/bsPeer.server.test.js
+++ test/bsPeer.server.test.js
@@ -74,6 +74,10 @@ describe('BSPeer:\n', function(){
 		testClient.on('message', checkPeeringReply);
     });
     
+    it('Should fetch the correct interface list for forwarding', function(done) {
+		done();
+	});
+    
     it('Should broadcast a peering request if pool is full', function(done) {
 		var conn1 = new WebSocket('ws://0.0.0.0:5000');
 		conn1.on('message', function(data) {
@@ -166,7 +170,7 @@ describe('BSPeer:\n', function(){
 	});
 
     it('Should reject a packet with every peer ids of the connection pool into the path', function(done) {
-	    var injectedPath = [];	
+	    var injectedPath = [];
 	    var conn1 = new WebSocket('ws://0.0.0.0:5000');
 		conn1.on('message', function(data) {
 			//console.log('con1 : ' + data);			
